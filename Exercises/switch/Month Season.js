53195b63e575ede8d273f355a63b8c696bbfe745
Moazzem Hossain <48817696+hossainmoazzem@users.noreply.github.com>
2020-09-28 01:20:25 +1000

    Create Month Season.js
    This code gives the user the season depending on the month is selected.

---
+++ Exercises/switch/Month Season.js
@@ -0,0 +1,59 @@
+var month = 'Sept';
+var Season = '';
+
+switch (month){
+  case 'Dec':
+  season = 'Summer'
+  break;
+
+  case 'Jan':
+  season = 'Summer'
+  break;
+
+  case 'Feb':
+  season = 'Summer'
+  break;
+
+  case 'Mar':
+  season = 'Autumn'
+  break;
+
+  case 'Apr':
+  season = 'Autumn'
+  break;
+  
+  case 'May':
+  season = 'Autumn'
+  break;
+
+  case 'Jun':
+  season = 'Winter'
+  break;
+
+  case 'July':
+  season = 'Winter'
+  break;
+
+  case 'Aug':
+  season = 'Winter'
+  break;
+
+  case 'Sept':
+  season = 'Spring'
+  break;
+
+  case 'Oct':
+  season = 'Spring'
+  break;
+
+  case 'Nov':
+  season = 'Spring'
+  break;
+
+  case 'Dec':
+  season = 'Spring'
+  break;
+
+}
+
+gs.info("It's Now the month of " + month + ' and its ' + season + ' in Australia.') 
