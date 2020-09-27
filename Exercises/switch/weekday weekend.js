d87adfb77d2178c99a3f87aa9f8eaea3586a9b17
Moazzem Hossain <48817696+hossainmoazzem@users.noreply.github.com>
2020-09-28 01:37:45 +1000

    Create weekday weekend.js
    This code gives user the weekday or weekend depending on the day of the week is selected.

---
+++ Exercises/switch/weekday weekend.js
@@ -0,0 +1,41 @@
+var day = 'Monday';
+var typeOfDay ='';
+
+switch(day) {
+  case 'Monday':
+  typeOfDay='Weekday';
+  break;
+
+
+  case 'Tuesday':
+  typeOfDay='Weekday';
+  break;
+
+
+  case 'Wednesday':
+  typeOfDay='Weekday';
+  Break;
+
+
+  case 'Thursday':
+  typeOfDay='Weekday';
+  break;
+
+
+  case 'Friday':
+  typeOfDay='Weekday';
+  break;
+
+  case 'Saturday':
+  typeOfDay='Weekend';
+  break;
+
+
+  case 'Sunday':
+  typeOfDay='Weekend';
+  break;
+
+
+}
+
+gs.info("If it's " + day + " then it's " + typeOfDay + '.' )
